54c062d5152c0d05838761abe8b56f8ca5d144b2
Ivan Komarenko <54206076+i1garde@users.noreply.github.com>
2021-05-30 18:45:06 +0300

    Finish Lab6

---
+++ Lab6/SP-Lab6/QTCPServer/mainwindow.h
@@ -0,0 +1,54 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QDebug>
+#include <QFile>
+#include <QFileDialog>
+#include <QMessageBox>
+#include <QMetaType>
+#include <QSet>
+#include <QStandardPaths>
+#include <QTcpServer>
+#include <QTcpSocket>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = nullptr);
+    qint64 dirSize(QString dirPath);
+    QString formatSize(qint64 size);
+    ~MainWindow();
+
+signals:
+    void newMessage(QString);
+private slots:
+    void newConnection();
+    void appendToSocketList(QTcpSocket* socket);
+
+    void readSocket();
+    void discardSocket();
+    void displayError(QAbstractSocket::SocketError socketError);
+
+    void displayMessage(const QString& str);
+    void sendMessage(QTcpSocket* socket, QString message);
+    void sendAttachment(QTcpSocket* socket, QString filePath);
+
+    void on_pushButton_sendMessage_clicked();
+    void on_pushButton_sendAttachment_clicked();
+
+    void refreshComboBox();
+private:
+    Ui::MainWindow *ui;
+
+    QTcpServer* m_server;
+    QSet<QTcpSocket*> connection_set;
+};
+
+#endif // MAINWINDOW_H
